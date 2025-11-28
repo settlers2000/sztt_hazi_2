f73a16e3ce19c12382df0180ba0dd858edd0eb16
Jeges0 <janovszki84@gmail.com>
2025-11-28 02:45:12 +0100

    started user and admin classes

---
+++ Meterology/Meterology/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meterology
+{
+    internal class Admin : User
+    {
+        public Admin() { }
+
+        public void deleteAll()
+        {
+
+        }
+
+        public void changeUnit()
+        {
+
+        }
+    }
+}
---
+++ Meterology/Meterology/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meterology
+{
+    internal class User
+    {
+        public User()
+        {
+
+        }
+
+        public void loadData()
+        {
+
+        }
+
+        public void filerData()
+        {
+
+        }
+
+        public void showData()
+        {
+
+        }
+
+        public void analyseData()
+        {
+
+        }
+    }
+}
