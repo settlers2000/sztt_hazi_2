21cc6761f296896682733279ea4e76cc35c200b9
Jeges0 <janovszki84@gmail.com>
2025-11-28 03:36:57 +0100

    done some stuff to end for today

--- Meterology/Meterology/Program.cs
+++ Meterology/Meterology/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Meterology;
+using System.Linq.Expressions;
+
+List<Data> list = new List<Data>();
+Console.WriteLine("Hello, World!\n");
+
+string command;
+while ((command = Console.ReadLine().ToLower()) != "exit")
+{
+    switch (command)
+    {
+        case "start":
+            Console.WriteLine("Hello There!\n");
+            break;
+    }
+}
+Console.WriteLine("Bye");
+
