b7f83999cf2d7713d4bf55663badd4d9f5b57b6e
Jeges0 <janovszki84@gmail.com>
2025-11-28 02:59:36 +0100

    started Data class for easy import

--- Meterology/Meterology/Data.cs
+++ Meterology/Meterology/Data.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,23 @@ namespace Meterology
 {
     internal class Data
     {
+        private DateTime timestamp;
+        private double value;
+        private string unit;
+        private bool imported;
+        private string sensor;
+        public Data(DateTime timestamp, double value, string unit, bool imported, string sensor)
+        {
+            this.timestamp = timestamp;
+            this.value = value;
+            this.unit = unit;
+            this.imported = imported;
+            this.sensor = sensor;
+        }
+
+        public void changeUnit()
+        {
+
+        }
     }
 }
