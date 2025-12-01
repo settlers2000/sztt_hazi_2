0924ffe4b43ccf004dc195804f16c0e4a27f31a0
Jeges0 <janovszki84@gmail.com>
2025-12-01 23:57:04 +0100

    done data xml file

---
+++ Meterology/Meterology/FileManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meterology
+{
+    internal class FileManager
+    {
+        public void readTxtFile()
+        {
+            String temp;
+            StreamReader sr = new StreamReader("data.txt");
+            temp = sr.ReadLine();
+            while(temp != null)
+            {
+                Console.WriteLine(temp);
+                temp = sr.ReadLine();
+            }
+            sr.Close();
+            Console.ReadLine();
+        }
+
+        public void writeTxtFile() { }
+
+        public void importFromFile()
+        {
+
+        }
+    }
+}
