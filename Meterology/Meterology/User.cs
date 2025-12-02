ca2377d482785f8253d4a383fa4060629df861f2
Jeges0 <janovszki84@gmail.com>
2025-12-02 05:14:27 +0100

    im gonna die

--- Meterology/Meterology/User.cs
+++ Meterology/Meterology/User.cs
@@ -30,17 +30,25 @@ namespace Meterology
                 }
                 else if (input == "generate")
                 {
-                    string temp;
-                    Console.WriteLine("Generating Data...\nInput Time Interval (From-To):\nyy-mm-ddThh:mm:ss,yy-mm-ddThh:mm:ss");
-                    temp = Console.ReadLine();
-                    string[] token = temp.Split(",");
-                    DateTime[] time = { DateTime.Parse(token[0]), DateTime.Parse(token[1]) };
-                    Console.WriteLine("Input Number Of Data To Generate:");
-                    var num = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Input Value Range (1,200):");
-                    temp = Console.ReadLine();
-                    token = temp.Split(",");
-                    double[] range = { double.Parse(token[0]), double.Parse(token[1]) };
+                    try
+                    {
+                        string temp;
+                        Console.WriteLine("Generating Data...\nInput Time Interval (From-To):\nyy-mm-ddThh:mm:ss,yy-mm-ddThh:mm:ss");
+                        temp = Console.ReadLine();
+                        string[] token = temp.Split(",");
+                        DateTime[] time = { DateTime.Parse(token[0]), DateTime.Parse(token[1]) };
+                        Console.WriteLine("Input Number Of Data To Generate:");
+                        var num = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Input Value Range (1,200):");
+                        temp = Console.ReadLine();
+                        token = temp.Split(",");
+                        double[] range = { double.Parse(token[0]), double.Parse(token[1]) };
+                        generate(time, num, range);
+                    }
+                    catch(System.FormatException e)
+                    {
+                        Console.WriteLine("Wrong format used!");
+                    }
                     break;
                 }
                 else
@@ -65,6 +73,11 @@ namespace Meterology
         }
 
         public void analyseData()
+        {
+
+        }
+
+        public void generate(DateTime[] time, int num, double[] range)
         {
 
         }
