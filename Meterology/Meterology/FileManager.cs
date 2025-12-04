492440319e8c8eb8189fd00fd5971c033456e856
Jeges0 <janovszki84@gmail.com>
2025-12-04 17:30:06 +0100

    fixed filemanager

--- Meterology/Meterology/FileManager.cs
+++ Meterology/Meterology/FileManager.cs
@@ -18,23 +18,35 @@ namespace Meterology
 
                 foreach (var node in database.Root.Elements("Data"))
                 {
-                    //var timestamp = DateTime.ParseExact(node.Element("Timestamp").Value, "F", null);
-                    var timestamp = DateTime.Parse(node.Element("Timestamp").Value);
-                    var value = double.Parse(node.Element("Value").Value);
-                    var unit = node.Element("Unit").Value;
-                    var source = node.Element("Source").Value;
-                    var sensor = node.Element("Sensor")?.Value;
+                    try
+                    {
+                        //var timestamp = DateTime.ParseExact(node.Element("Timestamp").Value, "F", null);
+                        var timestamp = DateTime.Parse(node.Element("Timestamp").Value);
+                        var value = double.Parse(node.Element("Value").Value);
+                        var unit = node.Element("Unit").Value;
+                        var source = node.Element("Source").Value;
+                        var sensor = node.Element("Sensor")?.Value;
 
-                    Data data = new Data(timestamp, value, unit, source == "imported" ? true : false, sensor);
-                    list.Add(data);
-                    numberOfDataLoaded++;
+                        Data data = new Data(timestamp, value, unit, source == "imported" ? true : false, sensor);
+                        list.Add(data);
+                        numberOfDataLoaded++;
+                    }
+                    catch (System.FormatException e)
+                    {
+                        numberOfDataLost++;
+                    }                
                 }
 
                 list.Sort(delegate (Data x, Data y)
                 {
                     return x.timestamp.CompareTo(y.timestamp);
                 });
-                Console.WriteLine("Imported successfully!");
+
+                if(numberOfDataLost > 0)
+                {
+                    Console.WriteLine("Wrong format error!\nSuccessfully loaded data: " + numberOfDataLoaded + "\nFailed data: " + numberOfDataLost);
+                }
+                else { Console.WriteLine("Imported successfully!"); }
             }
             catch(FileNotFoundException e)
             {
@@ -47,11 +59,6 @@ namespace Meterology
             catch(System.Xml.XmlException e)
             {
                 Console.WriteLine("File is empty!");
-            }
-            catch(System.FormatException e)
-            {
-                numberOfDataLost -= numberOfDataLoaded; 
-                Console.WriteLine("Wrong format error!\nSuccessfully loaded data: " + numberOfDataLoaded + "\nFailed data: " +numberOfDataLost);
             }
             return list;
         }
